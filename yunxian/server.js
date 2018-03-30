dc3a53c49368fd8c4e2094fbf644b45445108e34
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2018-03-30 17:05:34 +0800

    no message

--- yunxian/server.js
+++ yunxian/server.js
@@ -28,6 +28,6 @@ app.use('/server', require('./serverApi'));
 
 
 
-app.listen(9981,()=>{
-    console.log('local:localhost:9981 上线的时候记得把端口改成8099， ws 改成8181');
+app.listen(8055,()=>{
+    console.log('local:localhost:8055 上线的时候记得把端口改成8099， ws 改成8181');
 });
