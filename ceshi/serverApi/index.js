0cf9f280fc2433d7195d43200bcffca61c738588
fengshuanmin <1262517900@qq.com>
2019-11-13 17:52:46 +0800

    no message

--- ceshi/serverApi/index.js
+++ ceshi/serverApi/index.js
@@ -360,9 +360,10 @@ router.get('/down',(req,res,next)=>{
             //在微信中打开
             // window.location.href='http://www.toumingxiuche.cn/tmxDocs/image.html'
             res.write(
-                '<script>' +
-                'window.open("http://www.toumingxiuche.cn/tmxDocs/image.html")'+
-                '</script>'
+                window.location.href="http://www.toumingxiuche.cn/tmxDocs/image.html"
+                // '<script>' +
+                // 'window.location.href="http://www.toumingxiuche.cn/tmxDocs/image.html"'+
+                // '</script>'
             );
         }else{
             vUrl='http://www.toumingxiuche.cn/app/version/app-official-releasetmxc_v1.3.3_officialtmxc_v1.3.3_official.apk'
