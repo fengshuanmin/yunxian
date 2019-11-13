74719fbc9b78bb975aced73828d8e1630c659bbe
fengshuanmin <1262517900@qq.com>
2019-11-13 17:47:22 +0800

    no message

--- ceshi/serverApi/index.js
+++ ceshi/serverApi/index.js
@@ -359,12 +359,9 @@ router.get('/down',(req,res,next)=>{
         if (/MicroMessenger/.test(ua)) {
             //在微信中打开
             // window.location.href='http://www.toumingxiuche.cn/tmxDocs/image.html'
-            {/*<script language="javascript">*/}
-                {/*window.location.href='http://www.toumingxiuche.cn/tmxDocs/image.html'*/}
-            {/*</script>*/}
             res.write(
                 '<script>' +
-                'window.location.href="http://www.toumingxiuche.cn/tmxDocs/image.html"'+
+                'window.open("http://www.toumingxiuche.cn/tmxDocs/image.html")'+
                 '</script>'
             );
         }else{
