9f750e0ef917bb5979f7ba83d0c71a5fdbfb328e
fengshuanmin <1262517900@qq.com>
2020-03-17 15:54:09 +0800

    no message

--- yunxian/serverApi/index.js
+++ yunxian/serverApi/index.js
@@ -286,19 +286,41 @@ router.post('/selectWXImg',(req,res,next)=>{
     }
     sql.Connect(query)
 })
+// router.get('/down',(req,res,next)=>{
+//     var ua = req.headers['user-agent'],vUrl
+//     if (/Android/.test(ua)){
+//         if (/MicroMessenger/.test(ua)) {
+//             //在微信中打开
+//             // window.location.href='http://www.toumingxiuche.cn/tmxDocs/image.html'
+//             vUrl='http://www.toumingxiuche.cn/tmxDocs/download.html'
+//         }else{
+//             vUrl='http://www.toumingxiuche.cn/app/version/app-official-releasetmxc_v1.3.3_officialtmxc_v1.3.3_official.apk'
+//         }
+//         //vUrl='https://www.pgyer.com/PKVF'
+//     }else{
+//         vUrl='https://itunes.apple.com/us/app/%E9%80%8F%E6%98%8E%E4%BF%AE%E8%BD%A6/id1271183608?l=zh&ls=1&mt=8'
+//     }
+//     res.write(
+//         '<script>' +
+//         'window.location.href="'+vUrl+'"'+
+//         '</script>'
+//     );
+//     console.log(vUrl);
+//     res.end();
+// })
 router.get('/down',(req,res,next)=>{
     var ua = req.headers['user-agent'],vUrl
     if (/Android/.test(ua)){
-        if (/MicroMessenger/.test(ua)) {
-            //在微信中打开
-            // window.location.href='http://www.toumingxiuche.cn/tmxDocs/image.html'
-            vUrl='http://www.toumingxiuche.cn/tmxDocs/download.html'
-        }else{
-            vUrl='http://www.toumingxiuche.cn/app/version/app-official-releasetmxc_v1.3.3_officialtmxc_v1.3.3_official.apk'
-        }
-        //vUrl='https://www.pgyer.com/PKVF'
+    if (/MicroMessenger/.test(ua)) {
+        //在微信中打开
+        // window.location.href='http://www.toumingxiuche.cn/tmxDocs/image.html'
+        vUrl='http://www.toumingxiuche.cn/tmxDocs/download.html'
     }else{
-        vUrl='https://itunes.apple.com/us/app/%E9%80%8F%E6%98%8E%E4%BF%AE%E8%BD%A6/id1271183608?l=zh&ls=1&mt=8'
+        vUrl='http://www.toumingxiuche.cn/app/version/app-official-releasetmxc_v1.3.3_officialtmxc_v1.3.3_official.apk'
+    }
+    // vUrl1='https://www.pgyer.com/1yCE'
+    }else{
+        vUrl='https://itunes.apple.com/us/app/%E9%80%8F%E6%98%8E%E4%BF%AE%E8%BD%A6-%E4%B8%AA%E4%BA%BA%E8%AF%A2%E4%BB%B7/id1435034638?l=zh&ls=1&mt=8'
     }
     res.write(
         '<script>' +
